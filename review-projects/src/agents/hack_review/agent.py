1edccf7621375e94b91004e99c9bddc21e095da7
Justin Harris <juharris@users.noreply.github.com>
2025-09-28 16:31:18 -0400

    2.5-flash-lite

--- review-projects/src/agents/hack_review/agent.py
+++ review-projects/src/agents/hack_review/agent.py
@@ -1,8 +1,12 @@
 from google.adk.agents import Agent
 
+# Models: https://ai.google.dev/gemini-api/docs/models
+# model = "gemini-2.5-flash"
+model = "gemini-2.5-flash-lite"
+
 root_agent = Agent(
     name="root_agent",
-    model="gemini-2.0-flash",
+    model=model,
     description="The root agent for the hackathon project review system",
     instruction="You are a helpful assistant that reviews hackathon projects and rates them based on a set of criteria.",
 )
